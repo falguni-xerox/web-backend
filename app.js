79d599f82fc0cb74b4b6f8a0b9cf07b795b8290f
falguni-xerox <sunilthakor3693@gmail.com>
2026-08-02 08:43:35 +0530

    Fix CORS for Vercel frontend

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.use(
       "http://localhost:5173",
       "https://web-frontend-sigma-orcin.vercel.app",
     ],
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "OPTIONS",
-    ],
-    credentials: true,
+methods: [
+  "GET",
+  "POST",
+  "PUT",
+  "PATCH",
+  "DELETE",
+  "OPTIONS",
+],dentials: true,
   })
 );
 
